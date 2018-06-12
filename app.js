8bfe644044d5d6f05e39a2bbe6e735799aed05c5
nxy <xiaoyang91315@sina.com>
2018-06-12 21:27:04 +0800

    commit

--- app.js
+++ app.js
@@ -34,6 +34,23 @@ App({
       }
     })
   },
+
+  hasToken() {
+    let token = wx.getStorageSync('token') ? true : false
+    return token
+  },
+  getToken() {
+    let token = wx.getStorageSync('token') ? wx.getStorageSync('token') : null
+    return token
+  },
+
+  /**
+   * get请求拦截
+   * 
+   * @header[version]           版本号
+   * @header[access-token]      验签
+   * @header[user-token]        验签
+   */
   get(url, params, fn, finalFn){
     let arr = []
     let paramsStr = ''
@@ -43,7 +60,6 @@ App({
       }
       paramsStr = '?' + arr.join('&')
     }
-    
     
     wx.request({
       method: 'get',
@@ -61,6 +77,14 @@ App({
       }
     })
   },
+
+  /**
+   * post请求拦截 
+   * 
+   * @header[version]           版本号
+   * @header[access-token]      验签
+   * @header[user-token]        验签
+   */
   post(url, params, fn) {
     wx.request({
       method: 'post',
@@ -72,11 +96,52 @@ App({
       },
       data: params,
       success: data => {
-        fn && fn()
+        fn && fn(data.data.data)
       },
     })
   },
+
+  /**
+   * 用户登录
+   */
+  login() {
+    
+  },
+
+  /**
+   * 获取用户信息
+   */
+  getUserInfo() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回  
+      // 所以此处加入 callback 以防止这种情况  
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理  
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }  
+  },
+  
   globalData: {
+    hasUserInfo: false,
     userInfo: null,
     reqUrl: 'https://api.youcanwuchu.com',//请求域名
     imgUrl: 'http://www.youcanwuchu.com/Public/Uploads/',//图片拼接路径
